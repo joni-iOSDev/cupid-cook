35b4761dae374ccb7e665f205b04dc3d588cc68b
Jonatan Gonzalez <36254816+joni-iOSDev@users.noreply.github.com>
2022-10-25 19:37:34 -0300

    Initial Commit

---
+++ CupidCook/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CupidCook
+//
+//  Created by Joni Gonzalez on 25/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupidCook/CupidCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupidCookApp.swift
+//  CupidCook
+//
+//  Created by Joni Gonzalez on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupidCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
