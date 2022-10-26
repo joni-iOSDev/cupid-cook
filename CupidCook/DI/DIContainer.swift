365cc8e197364119f65af8ae95982b400ad01a9f
Jonatan Gonzalez <36254816+joni-iOSDev@users.noreply.github.com>
2022-10-25 21:18:20 -0300

    added DI resolver

---
+++ CupidCook/DI/DIContainer.swift
@@ -0,0 +1,17 @@
+//
+//  DIContainer.swift
+//  CupidCook
+//
+//  Created by Joni Gonzalez on 25/10/2022.
+//
+
+import Foundation
+import Swinject
+
+func buildContainer() -> Container {
+    let container = Container()
+    
+    // TODO: Add dependecies
+    
+    return container
+}
